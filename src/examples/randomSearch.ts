2cd78879c609cb44518a95fe2e6a3b88eaf87968
Nestor Torres <alu0101021768@ull.edu.es>
2022-06-30 23:20:14 +0100

    chore: updated random search example

--- src/examples/randomSearch.ts
+++ src/examples/randomSearch.ts
@@ -1,16 +1,16 @@
 import { randomSearch } from '../algorithms/RandomSearch';
 import { ExecutionEngine } from '../enums/ExecutionEngine';
 
-const { candidate: docker } = randomSearch({
+const { candidate: dockerCandidate } = randomSearch({
   engine: ExecutionEngine.DOCKER,
   numberOfIterations: 1,
 });
-const { candidate: rscript } = randomSearch({
+const { candidate: rscriptCandidate } = randomSearch({
   engine: ExecutionEngine.RSCRIPT,
   numberOfIterations: 1,
 });
 
-console.log(`Best docker candidate parameters: [${docker.parameters}]`);
-console.log(`Best docker candidate output: ${docker.output}`);
-console.log(`Best rscript candidate parameters: [${rscript.parameters}]`);
-console.log(`Best rscript candidate output: ${rscript.output}`);
+console.log(`Best docker candidate parameters: [${dockerCandidate.parameters}]`);
+console.log(`Best docker candidate output: ${dockerCandidate.output}`);
+console.log(`Best rscript candidate parameters: [${rscriptCandidate.parameters}]`);
+console.log(`Best rscript candidate output: ${rscriptCandidate.output}`);
